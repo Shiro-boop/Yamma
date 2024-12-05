cf2cd292792818607deacfefe466cc8e3cbe942c
Alexander <nellishiro@gmail.com>
2024-12-05 08:41:29 +0200

    add burger menu

---
+++ js/page.js
@@ -0,0 +1,5 @@
+let burgerBtn = document.querySelector('.burger__menu');
+
+burgerBtn.addEventListener('click', (e)=> {
+    burgerBtn.classList.toggle('active')
+})
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let authBtn = document.querySelector('.auth_button');
 
+
 authBtn.addEventListener('click', (e)=> {
     e.preventDefault();
     window.location.href = 'page.html';
